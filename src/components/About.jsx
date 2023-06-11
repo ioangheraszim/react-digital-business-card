e5cab0985ee1a361b619c729fdab91f9d3ea6bab
ioangheraszim <gijohny17@gmail.com>
2023-06-11 12:15:36 +0100

    added components and styles.css to the project

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About () {
+    return (
+        <div className="abouts">
+            <h2 className="ab-header">About</h2>
+            <p className="para">Mount Fuji is the tallest mountain in Japan standing at 3,775 meters (12,380feet). 
+            Mount Fuji is the single most popular tourist site in Japan for both Japanese and foreign tourists</p>
+        </div>
+    )
+}
